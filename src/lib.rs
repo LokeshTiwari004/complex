c65610f32831e5704f5d672a17dae6ae28ab8230
LokeshTiwari004 <lokeshtiwari001vns@gmail.com>
2022-06-03 23:31:45 +0530

    added ComplexNumber Definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+pub mod complex {
+    pub struct ComplexNumber {
+        real: f64,
+        imag: f64,
+    }
+
+    impl ComplexNumber {
+        pub fn real(&self) -> f64 {
+            self.real
+        }
+
+        pub fn imag(&self) -> f64 {
+            self.imag
+        }
     }
 }
