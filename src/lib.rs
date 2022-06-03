679c7b13085757e0320944ce83d8ceb503bd58ad
LokeshTiwari004 <lokeshtiwari001vns@gmail.com>
2022-06-04 00:28:24 +0530

    implemented addition of Complex Numbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,48 @@
 pub mod complex {
-    #[derive(Debug)]
+    #[derive(Debug, Clone, Copy)]
     pub struct ComplexNumber {
         real: f64,
         imag: f64,
     }
 
+    // getters and setters
     impl ComplexNumber {
-
-        pub fn new(real: f64, imag: f64) -> ComplexNumber {
-            ComplexNumber { real, imag }
-        }
-
         pub fn real(&self) -> f64 {
             self.real
         }
 
         pub fn imag(&self) -> f64 {
             self.imag
+        }
+
+        pub fn set_real(&mut self, set_with: f64) {
+            self.real = set_with;
+        }
+
+        pub fn set_imag(&mut self, set_with: f64) {
+            self.imag = set_with;
+        }
+    }
+
+    // Methods defining Operations with Complex Numbers 
+    impl ComplexNumber {
+        pub fn add(&mut self, add_with: &ComplexNumber) {
+            self.real += add_with.real;
+            self.imag += add_with.imag;
+        }
+    }
+
+    // Associated Functions for { creating Instances, defining Complex Number Operations}
+    impl ComplexNumber {
+        pub fn new(real: f64, imag: f64) -> ComplexNumber {
+            ComplexNumber { real, imag }
+        }
+
+        pub fn addition_of(num1: &ComplexNumber, num2: &ComplexNumber) -> ComplexNumber {
+            ComplexNumber {
+                real: (num1.real + num2.real),
+                imag: (num1.imag + num2.imag)
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use complex::complex::ComplexNumber;
 
 fn main() {
-  let z = ComplexNumber::new(2.0, 1.0);
+  let mut z1 = ComplexNumber::new(2.0, 1.0);
+  let z2 = ComplexNumber::new(2.0, 1.0);
 
-  println!("z is {:#?}", z);
+  println!("z is {:?}", ComplexNumber::addition_of(&z1, &z2));
+
+  z1.add(&z2);
+  println!("z is {:?}", z1);
+
 }
