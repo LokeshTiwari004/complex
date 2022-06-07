aa598cad3f9c83055a27667512394da61e341bb2
LokeshTiwari004 <lokeshtiwari001vns@gmail.com>
2022-06-07 12:36:56 +0530

    .

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,19 @@ use std::time::Instant;
 use complex::complex::ComplexNumber;
 
 fn main() {
-  let num3 = ComplexNumber::cartesian(0.0, 1.0);
-  let start3 = Instant::now();
-  let i_i3 = ComplexNumber::exp_of(&num3, &num3);
-  let duration3 = start3.elapsed();
-  
-  let num1 = ComplexNumber::cartesian(0.0, 1.0);
+  let mut num1 = ComplexNumber::cartesian(21.0, 19.0);
+  let num2 = ComplexNumber::cartesian(10.0, 17.0);
+
   let start1 = Instant::now();
   let i_i1 = ComplexNumber::exp_of(&num1, &num1);
   let duration1 = start1.elapsed();
 
+  let start3 = Instant::now();
+  num1.exp(&num2);
+  let duration3 = start3.elapsed();
+  
 
-  println!("i^i is {:#?}. \nTime Elapsed = {:?}", i_i3, duration3);
+
   println!("i^i is {:#?}. \nTime Elapsed = {:?}", i_i1, duration1);
+  println!("i^i is {:#?}. \nTime Elapsed = {:?}", &num1, duration3);
 }
