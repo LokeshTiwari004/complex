1b6a83008859f01fed8f8741fa600df70b5f346b
LokeshTiwari004 <lokeshtiwari001vns@gmail.com>
2022-06-07 12:05:52 +0530

    .

--- src/complex.rs
+++ src/complex.rs
@@ -4,34 +4,19 @@ use std::f64::consts::E;
 #[derive(Debug, Clone, Copy)]
 pub struct ComplexNumber {
     real: f64,
-    imag: f64,
-    modulus: f64,
-    argument: f64,
+    imag: f64
 }
 
 impl ComplexNumber {
     pub fn cartesian(real: f64, imag: f64) -> ComplexNumber {
-        let mut argument = (imag / real).atan();
-        if real.signum() == -1.0 {
-            if argument.signum() == 1.0 {
-              argument -= PI;
-            } else {
-              argument += PI;
-            }
-        };
-
         ComplexNumber {
             real,
-            imag,
-            modulus: (real.powi(2) + imag.powi(2)).sqrt(),
-            argument,
+            imag
         }
     }
 
     pub fn polar(modulus: f64, argument: f64) -> ComplexNumber {
         ComplexNumber {
-            modulus,
-            argument,
             real: modulus * argument.cos(),
             imag: modulus * argument.sin(),
         }
@@ -46,66 +31,25 @@ impl ComplexNumber {
     pub fn imag(&self) -> f64 {
         self.imag
     }
-
-    pub fn modulus(&self) -> f64 {
-        self.modulus
-    }
-
-    pub fn argument(&self) -> f64 {
-        self.argument
-    }
 }
 
 impl ComplexNumber {
     pub fn set_real(&mut self, set_with: f64) {
       self.real = set_with;
-      self.modulus = (self.imag.powi(2) + self.real.powi(2)).sqrt();
-
-      let mut argument = (self.imag / self.real).atan();
-      if self.real.signum() == -1.0 {
-          if argument.signum() == 1.0 {
-            argument -= PI;
-          } else {
-            argument += PI;
-          }
-      };
-      self.argument = argument;
     }
 
     pub fn set_imag(&mut self, set_with: f64) {
       self.imag = set_with;
-      self.modulus = (self.imag.powi(2) + self.real.powi(2)).sqrt();
-      
-      let mut argument = (self.imag / self.real).atan();
-      if self.real.signum() == -1.0 {
-          if argument.signum() == 1.0 {
-            argument -= PI;
-          } else {
-            argument += PI;
-          }
-      };
-      self.argument = argument;
-    }
-
-    pub fn set_modulus(&mut self, set_with: f64) {
-      self.modulus = set_with;
-      self.real = self.modulus * self.argument.cos();
-      self.imag = self.modulus * self.argument.sin()
-    }
-    
-    pub fn set_argument(&mut self, set_with: f64) {
-      self.argument = set_with;
-      self.real = self.modulus * self.argument.cos();
-      self.imag = self.modulus * self.argument.sin()
     }
 }
 
 
 impl ComplexNumber {
-  pub fn add(&mut self, add_with: &ComplexNumber) {
-    self.real += add_with.real;
-    self.imag += add_with.imag;
-    
+  pub fn modulus(&self) -> f64 {
+    (self.real.powi(2) + self.imag.powi(2)).sqrt()
+  }
+
+  pub fn argument(&self) -> f64 {
     let mut argument = (self.imag / self.real).atan();
     if self.real.signum() == -1.0 {
         if argument.signum() == 1.0 {
@@ -114,48 +58,54 @@ impl ComplexNumber {
           argument += PI;
         }
     };
-    self.argument = argument;
-    self.modulus = (self.imag.powi(2) + self.real.powi(2)).sqrt();
+    argument
+  }
+
+  
+  pub fn add(&mut self, add_with: &ComplexNumber) {
+    self.real += add_with.real;
+    self.imag += add_with.imag;
   }
   
   pub fn subtract(&mut self, subtract_with: &ComplexNumber) {
     self.real -= subtract_with.real;
     self.imag -= subtract_with.imag;
-    
-    let mut argument = (self.imag / self.real).atan();
-    if self.real.signum() == -1.0 {
-        if argument.signum() == 1.0 {
-          argument -= PI;
-        } else {
-          argument += PI;
-        }
-    };
-    self.argument = argument;
-    self.modulus = (self.imag.powi(2) + self.real.powi(2)).sqrt();
   }
 
   pub fn multiply(&mut self, multiply_with: &ComplexNumber) {
-    self.modulus *= multiply_with.modulus;
-    self.argument += multiply_with.argument;
+    let real = self.real * multiply_with.real - self.imag * multiply_with.imag;
+    let imag = self.real * multiply_with.imag + self.imag * multiply_with.real;
 
-    self.real = self.modulus * self.argument.cos();
-    self.imag = self.modulus * self.argument.sin();
+    self.real = real;
+    self.imag = imag;
   }
 
-  pub fn exponentiation(&mut self, index: &ComplexNumber) {
-    let argument = if self.argument.abs() > PI {
-        self.argument % PI 
-      } else {
-        PI
-      };
-    
-    let r = self.modulus.log(E);
+  pub fn log(&mut self, base: f64) {
+    let argument = self.argument();
+    let modulus = self.modulus();
 
-    self.modulus = (index.real * r - argument * index.imag).exp();
-    self.argument = index.real * argument + r * index.imag;
+    self.real = modulus.log(base);
+    self.imag = argument * E.log(base);
+  }
 
-    self.real = self.modulus * self.argument.cos();
-    self.imag = self.modulus * self.argument.sin();
+  pub fn exp(&mut self, index: &ComplexNumber) {
+    let imag = self.argument();
+    let real = self.modulus().log(E);
+
+    let ans_modulus = (index.real * real - imag * index.imag).exp();
+    let ans_argument = index.real * imag + real * index.imag;
+
+    self.real = ans_modulus * ans_argument.cos();
+    self.imag = ans_modulus * ans_argument.sin();
+
+    // self.log(E);
+    // self.multiply(index);
+
+    // let ans_mod = self.real.exp();
+    // let ans_argument = self.imag;
+
+    // self.real = ans_mod * ans_argument.cos();
+    // self.imag = ans_mod * ans_argument.sin();
   }
 }
 
@@ -178,9 +128,15 @@ impl ComplexNumber {
       ans
     }
 
-    pub fn exponentiation_of(base: &ComplexNumber, power: &ComplexNumber) -> ComplexNumber {
+    pub fn exp_of(base: &ComplexNumber, power: &ComplexNumber) -> ComplexNumber {
       let mut ans = base.clone();
-      ans.exponentiation(power);
+      ans.exp(power);
+      ans
+    }
+
+    pub fn log_of(num: &ComplexNumber, base: f64) -> ComplexNumber {
+      let mut ans = num.clone();
+      ans.log(base);
       ans
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use complex::complex::ComplexNumber;
 fn main() {
   let num3 = ComplexNumber::cartesian(0.0, 1.0);
   let start3 = Instant::now();
-  let i_i3 = ComplexNumber::exponentiation_of(&num3, &num3);
+  let i_i3 = ComplexNumber::exp_of(&num3, &num3);
   let duration3 = start3.elapsed();
   println!("i^i is {:#?}. \nTime Elapsed = {:?}", i_i3, duration3);
 
   let num1 = ComplexNumber::cartesian(0.0, 1.0);
   let start1 = Instant::now();
-  let i_i1 = ComplexNumber::exponentiation_of(&num1, &num1);
+  let i_i1 = ComplexNumber::exp_of(&num1, &num1);
   let duration1 = start1.elapsed();
   println!("i^i is {:#?}. \nTime Elapsed = {:?}", i_i1, duration1);
 }
